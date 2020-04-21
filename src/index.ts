293c6021c10a41ffb0e0d52a348e5f05d5db2d4e
Denis Tuzhik <dev@dentuzhik.com>
2020-04-21 17:07:39 +0300

    First attempt to implement an action

--- src/index.ts
+++ src/index.ts
@@ -1,107 +1,70 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
-import * as exec from '@actions/exec';
-import fetch from 'node-fetch';
 
-const escapeShell = function(command:string) {
-  return command.replace(/(["\s'#$`\\])/g,'\\$1');
+interface Options {
+  issuePrefix: string;
+  issueBaseUrl: string;
+}
+
+const prBodyIssuesReplaceRegex = new RegExp(
+  '[//]:s?#s?(autolink_jira_issues_start)[sS]+[//]:s?#s?(autolink_jira_issues_end)',
+  'gm',
+);
+
+const extractIssuesKeysFromBranch = (prBranchName: string, options: Options) => {
+  const [_prefix, _description, ...jiraIssues] = prBranchName.split('/');
+  return jiraIssues.filter((issue) => issue.startsWith(options.issuePrefix));
 };
+
+const createIssueLink = (issueKey: string, issueBaseUrl: string) => {
+  return `${issueBaseUrl}/${issueKey}`;
+};
+
+function autolinkIssues(prBranchName: string, prBody: string | undefined, options: Options) {
+  if (!prBody) return;
+
+  const issuesKeys = extractIssuesKeysFromBranch(prBranchName, options);
+  const issuesLinks = issuesKeys.map((issueKey) => createIssueLink(issueKey, options.issueBaseUrl));
+  const issuesLinksFormatted = issuesLinks.join('\n');
+
+  return prBody.replace(prBodyIssuesReplaceRegex, issuesLinksFormatted);
+}
 
 async function run() {
   try {
-    if (!github.context.payload.issue) {
+    if (!github.context.payload.issue || !github.context.payload.pull_request) {
       throw {
-        message: "This action can only be executed from PR or Issue"
-      }
-    } 
-    let pullRequestApiUrl:string = github.context.payload.issue.pull_request.url;
-    const githubApiToken:string = core.getInput('github-token');
-    const githubUserName:string = core.getInput('github-user-name');
-    const githubUserEmail:string = core.getInput('github-user-email');
-    let tag:string = core.getInput('tag');
-    const shouldTagBaseBranch:string = core.getInput('should-tag-base-branch');
-
-    if (githubApiToken !== null) {
-      pullRequestApiUrl = pullRequestApiUrl.replace("api.github.com", `${githubApiToken}@api.github.com`);
+        message: 'This action can only be executed from PR or Issue',
+      };
     }
 
-    const pullRequest = await fetch(pullRequestApiUrl).then(data => data.json())
+    const githubApiToken: string = core.getInput('github-token');
+    const issueBaseUrl = core.getInput('issue-base-url', { required: true });
+    const issuePrefix = core.getInput('issue-prefix', { required: true });
+    const pullRquestBranchName = github.context.payload.pull_request.head.ref;
+    const pullRequestBody = github.context.payload.pull_request?.body;
 
-    let pullRequestHtmlUrl:string = pullRequest.base.repo.html_url;
-
-    if (githubApiToken !== null) {
-      pullRequestHtmlUrl = pullRequestHtmlUrl.replace("github.com", `${githubApiToken}@github.com`);
-    }
-
-    const headRef:string = pullRequest.head.ref;
-    const baseRef:string = pullRequest.base.ref;
-
-    if (githubUserName) {
-      await exec.exec(`git config --global user.name "${githubUserName}"`);
-    }
-
-    if (githubUserEmail) {
-      await exec.exec(`git config --global user.email "${githubUserEmail}"`);
-    }
-    
-    await exec.exec(`git checkout ${baseRef}`);
-
-    console.log(`\n\n Merging ${headRef} into ${baseRef} using '--strategy-option theirs'`);
-
-    await exec.exec(`git merge origin/${headRef} --allow-unrelated-histories --strategy-option theirs`);
-
-    console.log(`\n\n Checking for deleted files in ${headRef}`);
-
-    let conflictFiles = '';
-    const gitDiffListeners = {
-      stdout: (data: Buffer) => {
-        conflictFiles += data.toString();
-      }
+    const updateRequest = {
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      pull_number: github.context.payload.pull_request.number,
+      body: autolinkIssues(pullRquestBranchName, pullRequestBody, { issuePrefix, issueBaseUrl }),
     };
 
-    await exec.exec(`git diff --name-status --diff-filter=A origin/${headRef}`, undefined, { listeners: gitDiffListeners });
+    const client = new github.GitHub(githubApiToken);
+    // @ts-ignore
+    const response = await client.pulls.update(updateRequest);
 
-    const filesSeparator = new RegExp(/A\t/g);
-    console.log(`\n\nFiles that was removed in ${headRef}, but still exist in ${baseRef}:\n${conflictFiles.replace(filesSeparator, '')}`);
-    if (conflictFiles.trim().length != 0) {
-      console.log(`\n\nRemoving conflicted files from ${baseRef}`);
-      await exec.exec(`git rm ${conflictFiles.replace(filesSeparator, ' ').replace(/\n/g, '')}`);
-      await exec.exec(`git commit --amend --no-edit`);
+    core.info(`response: ${response}`);
+    if (response.status !== 200) {
+      core.error('Updating pull request has failed');
     }
 
-    await exec.exec(`git push ${pullRequestHtmlUrl}`);
-
-    if (shouldTagBaseBranch) {
-      if (!tag) {
-        let myOutput = '';
-        let myError = '';
-    
-        const options = {
-          listeners: {
-            stdout: (data: Buffer) => {
-              myOutput += data.toString();
-            },
-            stderr: (data: Buffer) => {
-              myError += data.toString();
-            }
-          }
-        }
-
-        await exec.exec("jq", ["-r", ".version", "package.json"], options);
-        tag = `v${myOutput.trim()}`;
-        myError && console.warn(myError)
-      }
-      console.log(`tag: ${tag}`);
-      await exec.exec(`git tag ${tag}`);
-      await exec.exec(`git push ${pullRequestHtmlUrl} ${tag}`);
-    }
-
-
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
+    const time = new Date().toTimeString();
+    core.setOutput('time', time);
   } catch (error) {
     core.setFailed(error.message);
   }
-};
+}
 
 run();
